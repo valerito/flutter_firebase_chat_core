76ff4416152340901b042415752f4957e05bb900
Samuel Valero <valero@compsaonline.com>
2023-01-18 09:53:16 +0100

    Add PartialVideo types

--- lib/src/firebase_chat_core.dart
+++ lib/src/firebase_chat_core.dart
@@ -351,6 +351,12 @@ class FirebaseChatCore {
         id: '',
         partialFile: partialMessage,
       );
+    } else if (partialMessage is types.PartialVideo) {
+      message = types.VideoMessage.fromPartial(
+        author: types.User(id: firebaseUser!.uid),
+        id: '',
+        partialVideo: partialMessage,
+      );
     } else if (partialMessage is types.PartialImage) {
       message = types.ImageMessage.fromPartial(
         author: types.User(id: firebaseUser!.uid),
